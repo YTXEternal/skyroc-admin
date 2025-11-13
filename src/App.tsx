1d5acc19d55c942cae652829e220ae9c3756c613
Ohh-889 <1509326266@qq.com>
2025-11-14 03:03:44 +0800

    feat(project): integrate @tanstack/react-query for data fetching and caching

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+
 import { RouterProvider } from '@/features/router';
 
 import { LazyAnimate } from './features/animate';
 import { AntdContextHolder, AntdProvider } from './features/antdConfig';
 import { LangProvider } from './features/lang';
 import { ThemeProvider } from './features/theme';
+import { queryClient } from './service/queryClient';
 
 const App = () => (
   <ThemeProvider>
-    <LangProvider>
-      <AntdProvider>
-        <AntdContextHolder>
-          <LazyAnimate>
-            <RouterProvider />
-          </LazyAnimate>
-        </AntdContextHolder>
-      </AntdProvider>
-    </LangProvider>
+    <QueryClientProvider client={queryClient}>
+      <LangProvider>
+        <AntdProvider>
+          <AntdContextHolder>
+            <LazyAnimate>
+              <RouterProvider />
+            </LazyAnimate>
+          </AntdContextHolder>
+        </AntdProvider>
+      </LangProvider>
+    </QueryClientProvider>
   </ThemeProvider>
 );
 
